3a23a9b84074349bf4e5f57cab2df32cf45666f8
FelipeSena02 <felipessena02@gmail.com>
2025-03-22 18:39:53 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Configurando rotas
+const produtoRoute = require('./routes/produtoRoute');
+app.use('/produtos', produtoRoute);
+const vendedorRoute = require('./routes/vendedorRoute');
+app.use('/vendedores', vendedorRoute);
+const clienteRoute = require('./routes/clienteRoute');
+app.use('/clientes', clienteRoute);
+
+// Porta de execução
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
